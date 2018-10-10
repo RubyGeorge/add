e36bf5e2012782c9ecae025c06ba60890976bc40
RubyGeorge <rubyreethageorge@gmail.com>
2018-10-10 12:10:18 +0530

    initial

---
+++ add/src/main/java/Addition.java
@@ -0,0 +1,53 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Addition
+ */
+public class Addition extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Addition() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    PrintWriter out;    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		out= response.getWriter(); 
+		String num1=(request.getParameter("no1"));
+		String num2=(request.getParameter("no2"));
+		out.println(printAdd(num1,num2));
+		
+		
+	}
+	public String printAdd(String num1, String num2)
+	{
+		try{
+			float num3=Float.parseFloat(num1);
+			float num4=Float.parseFloat(num2);
+		    float result=num3+num4;
+		    return (Float.toString(result));
+	}
+	catch(Exception e){
+		return("please type int or float");
+	}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */	
+
+}
---
+++ add/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Addition</h1>
+<form action="Add">
+<input type="text" name="no1">
+<input type="button" value="+">
+<input type="text" name="no2">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
